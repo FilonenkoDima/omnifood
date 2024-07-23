bc757f1b4a7eebaa722e86bcf1db656236278566
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-07-24 00:18:44 +0300

    implement mobile nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+// set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
 
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
