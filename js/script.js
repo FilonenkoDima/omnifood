16d06bff955ec3b46647203ae9d08afb51acf7ef
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-07-24 13:15:14 +0300

    add sticky navigation

--- js/script.js
+++ js/script.js
@@ -38,6 +38,28 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   let flex = document.createElement("div");
